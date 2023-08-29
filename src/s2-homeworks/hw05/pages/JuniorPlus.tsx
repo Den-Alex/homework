cdb6443d46197bf72ad361b9fbd9e378b791540b
alexDen <alexvanden@mail.ru>
2023-08-29 14:12:30 +0300

    14

--- src/s2-homeworks/hw05/pages/JuniorPlus.tsx
+++ src/s2-homeworks/hw05/pages/JuniorPlus.tsx
@@ -11,7 +11,7 @@ function JuniorPlus() {
         <div id={'hw5-page-junior-plus'}>
             <HW10 />
             <HW11 />
-            {/*<HW12 />*/}
+            <HW12 />
             {/*<HW13 />*/}
             {/*<HW14 />*/}
             {/*<HW15 />*/}
